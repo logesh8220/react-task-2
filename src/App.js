22386894ab8c29cf03eeb3c93f67ba176d745a42
logesh8220 <logeshthirumurugan@gmail.com>
2022-08-11 18:41:11 +0530

    save

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Card from './Card';
 import Cart from './Cart'
 import { useState } from 'react';
 import React from 'react';
-import { faStar} from '@fortawesome/free-solid-svg-icons'
-import { faStarHalfAlt} from '@fortawesome/free-solid-svg-icons'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStarHalfAlt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Navbar from './Navbar';
 import Header from './Header';
@@ -13,100 +13,100 @@ import Header from './Header';
 function App() {
   const data = [
     {
-      id : 1,
+      id: 1,
       title: "i Phone 13 mini",
       price: 70000,
       Ratings: <div>
-          <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-          <FontAwesomeIcon className='text-warning'icon={faStar}></FontAwesomeIcon>
-          <FontAwesomeIcon className='text-warning'icon={faStarHalfAlt}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStarHalfAlt}></FontAwesomeIcon>
       </div>,
       img: "https://m.media-amazon.com/images/I/61nPiOOv9BL._SL1500_.jpg"
     },
     {
-      id : 2,
+      id: 2,
       title: "i Phone 13 ",
       price: 98000,
       Ratings: <div>
-      <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStarHalfAlt}></FontAwesomeIcon>
-  </div>,
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStarHalfAlt}></FontAwesomeIcon>
+      </div>,
       img: "https://m.media-amazon.com/images/I/617FFRO3vcL._SL1500_.jpg"
     },
     {
-      id : 3,
+      id: 3,
       title: "i Phone 13 Pro",
       price: 120000,
       Ratings: <div>
-      <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStarHalfAlt}></FontAwesomeIcon>
-  </div>,
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStarHalfAlt}></FontAwesomeIcon>
+      </div>,
       img: "https://m.media-amazon.com/images/I/61-r9zOKBCL._SL1500_.jpg"
     },
     {
-      id : 4,
+      id: 4,
       title: "i Phone 13 Pro Max",
       price: 140000,
       Ratings: <div>
-      <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStarHalfAlt}></FontAwesomeIcon>
-  </div>,
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStarHalfAlt}></FontAwesomeIcon>
+      </div>,
       img: "https://www.futureworldindia.in/image/cache/catalog/194253124351/611ovP2GkrL._SL1500_-1500x1500.jpg"
     },
     {
-      id : 5,
+      id: 5,
       title: "i Mac",
       price: 180000,
       Ratings: <div>
-      <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStarHalfAlt}></FontAwesomeIcon>
-  </div>,
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStarHalfAlt}></FontAwesomeIcon>
+      </div>,
       img: "https://m.media-amazon.com/images/I/810RI8O1jJL._SL1500_.jpg"
     },
     {
-      id : 6,
+      id: 6,
       title: "i Mac pro",
       price: 320000,
       Ratings: <div>
-      <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStar}></FontAwesomeIcon>
-      <FontAwesomeIcon className='text-warning'icon={faStar}></FontAwesomeIcon>
-      
-  </div>,
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-warning' icon={faStar}></FontAwesomeIcon>
+
+      </div>,
       img: "https://m.media-amazon.com/images/I/810RI8O1jJL._SL1500_.jpg"
     }
   ];
-  const [cart,setCart] = useState([])
-  const [total,setTotal] = useState(0)
+  const [cart, setCart] = useState([])
+  const [total, setTotal] = useState(0)
   let addToCart = (item) => {
-    setCart([...cart,item]);
+    setCart([...cart, item]);
     setTotal(+total + (item.price));
   }
 
   let removeCart = (item) => {
-   let index = cart.findIndex((Obj) => Obj.id === item.id);
-   cart.splice(index,1)
-   setCart([...cart])
-   setTotal(total - item.price)
+    let index = cart.findIndex((Obj) => Obj.id === item.id);
+    cart.splice(index, 1)
+    setCart([...cart])
+    setTotal(total - item.price)
 
   }
-  
+
 
   return (
-    
+
     <div className="container text-center">
       <Navbar></Navbar>
       <Header></Header>
@@ -126,14 +126,14 @@ function App() {
             {
               cart.length === 0 ? (
                 <div>No Items In Cart</div>
-              ) :(
-                cart.map((cartitem)=>{
-                  return <Cart cartitem = {cartitem} removeCart = {removeCart}></Cart>
+              ) : (
+                cart.map((cartitem) => {
+                  return <Cart cartitem={cartitem} removeCart={removeCart}></Cart>
                 })
               )
             }
             <ul class="list-group">
-              
+
               <li class="list-group-item d-flex justify-content-between align-items-center">
                 <b>Total</b>
                 <span className='mx-5'><b>₹{total}</b></span>
@@ -143,8 +143,8 @@ function App() {
         </div>
       </div>
       <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
-        </footer>
+        <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
+      </footer>
     </div>
   );
 }
